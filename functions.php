825d2a9f5392e132f02cae32e637e1ad750e1c42
Mitchell Bray <mitchellbray@CPE-60-229-39-232.lns7.ken.bigpond.net.au>
2015-03-11 09:50:24 +1100

    we have modals

--- functions.php
+++ functions.php
@@ -1,27 +1,50 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'parent_prefix_load_classes', 10 );
-
-function parent_prefix_load_classes()
+function generateCss()
 {
 	$primaryContrast = getContrastYIQ(tr_option_field("[primary]"));	
 
 	$vars = array();
+	//styles
 	$vars['primary'] = tr_option_field("[primary]");
 	$vars['primary_contrast'] = $primaryContrast;
 	$vars['rounding'] = tr_option_field("[rounding]");
-	
+	//fonts
 	$vars['base_font_size'] = tr_option_field("[base_font_size]");
 	$vars['small_heading'] = tr_option_field("[small_heading]");
 	$vars['medium_heading'] = tr_option_field("[medium_heading]");
 	$vars['large_heading'] = tr_option_field("[large_heading]");
 	$vars['huge_heading'] = tr_option_field("[huge_heading]");
+	//navs
+	switch (tr_option_field("[nav]")) {
+    case "none":
+        break;
+    case "fixed":
+        $vars['navFixed'] = true;
+        break;
+    case "trans":
+        $vars['navFixedTrans'] = true;
+        break;
+	}
 
 	$vars = array_filter($vars, 'strlen'); // removes unset array values
-// 	$plugins = ['forms'];
+	$plugins = array('svg');
 	
-	$css = csscrush_file(__DIR__.'/assets/css/site.css',array('vars'=>$vars, 'minify'=>false));
-	wp_enqueue_style ( 'site', $css, false);
+	$css = csscrush_file(__DIR__.'/assets/css/site.css',array('vars'=>$vars, 'minify'=>false, 'plugins'=>$plugins));
+}
+add_action( 'updateCss', 'generateCss');
+	
+add_action( 'wp_enqueue_scripts', 'loadCss', 10 );
 
+function loadCss()
+{
+	wp_enqueue_style ( 'site',get_stylesheet_directory_uri().'/assets/css/site.crush.css', false);
+	wp_enqueue_style ( 'icons',get_stylesheet_directory_uri().'/assets/css/font-icons.css', false);
 }
 
 add_filter('wp_nav_menu_items', 'do_shortcode'); // add shortcodes to menus
+
+if(isset($_SERVER['APPLICATION_ENV']) && $_SERVER['APPLICATION_ENV'] == 'development') {
+	add_action( 'wp_enqueue_scripts', 'generateCss', 10 );
+}else{
+	add_action( 'updateCss', 'generateCss');
+}
