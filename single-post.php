120e392b83bcd84512f8c2565942405a50179cc6
Mitchell Bray <mitchellbray@CPE-144-131-102-19.lns2.cht.bigpond.net.au>
2015-08-12 23:06:52 +1000

    fix

--- single-post.php
+++ single-post.php
@@ -4,9 +4,9 @@
 <body <?php body_class( $class ); ?>> 
 	<?php the_nav('');?>
 	<main>
-		<div class="container">
+		<div class="container panel-row-style">
 			<div class="post_content">
-				<?php the_title( '<h3>', '</h3>' ); ?>
+				<?php the_title( '<h1>', '</h1>' ); ?>
 				<?php the_content();?>
 			</div>
 		</div>
